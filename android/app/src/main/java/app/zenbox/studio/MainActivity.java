5c93ccfb943e1425f5a52b65d71d089ec983967f
LorEnzzhhz <qwertyuiop4874oe@gmail.com>
2026-08-11 16:22:20 +0800

    Sync android/app/src/main/java/app/zenbox/studio/MainActivity.java from Zenbox source

---
+++ android/app/src/main/java/app/zenbox/studio/MainActivity.java
@@ -0,0 +1,20 @@
+package app.zenbox.studio;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(BackgroundRunnerPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // "Exit" moves the app to the background instead of killing it, so
+        // streams and background work keep running (see KeepAliveService).
+        moveTaskToBack(true);
+    }
+}
